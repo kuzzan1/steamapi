c218b38d63dcbbfa8ace2efdf094d0611dde681d
Gustav Persson <gustav.persson@mobenga.com>
2016-05-09 12:16:40 +0200

    typescript och webpack och angular 2 motherfuckers

---
+++ app/src/app/about/about.spec.ts
@@ -0,0 +1,29 @@
+import {
+  it,
+  inject,
+  injectAsync,
+  describe,
+  beforeEachProviders
+} from '@angular/core/testing';
+import {TestComponentBuilder} from '@angular/compiler/testing';
+
+import {Component, provide} from '@angular/core';
+
+// Load the implementations that should be tested
+import {About} from './about.component';
+
+describe('About', () => {
+  // provide our implementations or mocks to the dependency injector
+  beforeEachProviders(() => [
+    About
+  ]);
+
+  it('should log ngOnInit', inject([ About ], (about) => {
+    spyOn(console, 'log');
+    expect(console.log).not.toHaveBeenCalled();
+
+    about.ngOnInit();
+    expect(console.log).toHaveBeenCalled();
+  }));
+
+});
